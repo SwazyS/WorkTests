00087f0a734c47e920ca4205e6c6613114529179
Vadim Genserovskiy <papeer337@gmail.com>
2021-09-24 20:15:42 +0600

    Initial Commit

---
+++ WorkTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkTests
+//
+//  Created by Vadim Genserovsky on 24.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
